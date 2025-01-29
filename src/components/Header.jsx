cd60c51d9b24723208686e57b45da3cb5f5f2e91
Ashwin <ashwinpalaniappanr@gmail.com>
2025-01-29 01:53:06 -0500

    modular css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import logo from '../assets/logo.png';
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/* inline styles */}
+      <p className={styles.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
